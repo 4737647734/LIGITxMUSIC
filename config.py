92ae0b35a0f08612a35ca8e1b80500389228497b
4737647734 <101376212+4737647734@users.noreply.github.com>
2025-07-31 21:12:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b3d0e737737d67a5bf5a5.jpg")
+PING_IMG = getenv("PING_IMG", "https://i.postimg.cc/439SG0NN/c399a1f82288c5824aae42881ef92d18.jpg")
+START_IMG = getenv("START_IMG", "https://i.postimg.cc/439SG0NN/c399a1f82288c5824aae42881ef92d18.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sastatony")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/team_ligit")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/quickbro")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/quickbro")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://i.postimg.cc/439SG0NN/c399a1f82288c5824aae42881ef92d18.jpg"
